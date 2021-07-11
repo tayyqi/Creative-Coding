d8f1c5aa777d836feb7b8967cb73cb2e65aaa4d1
tayyqi <yingqi99@hotmail.com>
2021-07-11 11:53:39 +0800

    initialise files

---
+++ Final Assignment - Interactive Collage/sketch.js
@@ -0,0 +1,21 @@
+//Creative Coding Final Assignment: Interactive Photo Collage
+
+/*Inspiration and sources
+British & Exotic Minerology by Nicholas Rougeux: https://www.c82.net/mineralogy/
+I was intrigued by the arrangement of the different minerals that seemed rather neat as a whole but upon zooming in, I noticed that the illustrations were of 
+irregular shape and did not specifically follow a grid format. The color palette was also harmonious, in addition to the intuitive mouse interactions. This 
+collage is of quite a large scale, consisting of 718 illustrations by James Sowerby, an early 19th-century British naturalist and mineralogist.
+
+Content: https://www.boredpanda.com/cat-maneuvers-explained-itsjayordan/?utm_source=google&utm_medium=organic&utm_campaign=organic
+I chanced upon an article about a vet who had pinned up witty explanations of cat bahaviours on his cork board. As the illustrations in the article are all in grayscale, 
+I will be sourcing for similar/new illustrations with the addition of gifs/videos to better illustrate the cat behaviours
+
+ */
+
+function setup() {
+    createCanvas(400,400);
+}
+
+function draw() {
+    background(230);
+}
